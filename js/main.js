315354c769323b6e2a358cdd7a6a9a33f7bfa8c8
AnitaMigacheva <55283508+AnitaMigacheva@users.noreply.github.com>
2020-02-27 16:01:31 +0300

    -

--- js/main.js
+++ js/main.js
@@ -1,16 +1,20 @@
 let canTop = document.body.querySelector('.can_top');
+let can = document.body.querySelector('.can');
+let bottle = document.body.querySelector('.bottle');
+let ball = document.body.querySelector('.ball');
+let collar = document.body.querySelector('.collar');
+let liquid = document.body.querySelector('.liquid');
+//
+// function flyBones(){
+//
+// }
 
-function flyBones() {
-
+function openCan(){
+  canTop.style.transform = 'rotate(-40deg) translate(10px, -50px)'
+  // flyBones()
 }
-
-function openCan() {
-  canTop.style.transform = "rotate(-40deg) translate(10px, -50px)"
-  // canTop.style.transform = "translateY(300px)"
-  flyBones()
-}
-
 
 function spill(){
-  
+  liquid.style.opacity = '1'
+  liquid.style.transform = 'scale(1)'
 }
