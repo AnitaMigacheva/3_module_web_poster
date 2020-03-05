4a3bea825b6b86198f4dd3ee35ba0607ac6ceb1f
AnitaMigacheva <55283508+AnitaMigacheva@users.noreply.github.com>
2020-03-06 01:50:59 +0300

    dd

--- js/main.js
+++ js/main.js
@@ -1,17 +1,27 @@
-let canTop = document.body.querySelector('.can_top');
-let can = document.body.querySelector('.can');
-let bottle = document.body.querySelector('.bottle');
-let ball = document.body.querySelector('.ball');
-let collar = document.body.querySelector('.collar');
-let liquid = document.body.querySelector('.liquid');
-let bones =
-//
-// function flyBones(){
-//  while(can.className == 'open')
-// }
+let canTop = document.body.querySelector('.can_top')
+let can = document.body.querySelector('.can')
+let bottle = document.body.querySelector('.bottle')
+let ball = document.body.querySelector('.ball')
+let collar = document.body.querySelector('.collar')
+let liquid = document.body.querySelector('.liquid')
+
+let bones = document.body.querySelectorAll('.bone')
+let bonesContainer = document.body.querySelector('.bones')
+
+can.addEventListener('click', () => {
+  can.classList.toggle("open")
+})
+
+function flyBones(){
+  
+  while(can.className == 'open')
+}
 
 function openCan(){
-  canTop.style.transform = 'rotate(-40deg) translate(10px, -50px)'
+  if (can.classList.contains('open')) {
+    canTop.style.transform = 'rotate(-40deg) translate(10px, -50px)'
+  }
+
 }
 
 function spill(){
