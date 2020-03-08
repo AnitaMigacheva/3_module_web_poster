e5643ab380eeb0c05c85e21924f89c6262059a77
AnitaMigacheva <55283508+AnitaMigacheva@users.noreply.github.com>
2020-03-08 23:42:01 +0300

    5

--- js/main.js
+++ js/main.js
@@ -36,51 +36,51 @@ let a = Array.from(balls)
 
 function showBalls() {
   ballsWrapper.style.display = 'block'
-  a.forEach(element => element.style.display = 'block')
+  a.forEach(element => element.style.display = 'inline-block')
 
 
-  function getRandomPosition(element) {
-    var x = document.body.offsetHeight-element.clientHeight;
-    var y = document.body.offsetWidth-element.clientWidth;
-    var randomX = Math.floor(Math.random()*x);
-    var randomY = Math.floor(Math.random()*y);
-    return [randomX,randomY]
-  }
+  // function getRandomPosition(element) {
+  //   var x = document.body.offsetHeight-element.clientHeight;
+  //   var y = document.body.offsetWidth-element.clientWidth;
+  //   var randomX = Math.floor(Math.random()*x);
+  //   var randomY = Math.floor(Math.random()*y);
+  //   return [randomX,randomY]
+  // }
+  //
+  //
+  //
+  // let i = 0
+  // let next = 1
+  // for (i = 0; i < a.length; i++) {
+  //   var xy = getRandomPosition(ballsWrapper);
+  //
+  //
+  //
+  //   a[i].style.width = mainBall.width;
+  //
+  //   a[i].style.top = xy[0] + 'px';
+  //   a[i].style.left = xy[1] + 'px';
+  //   a[i].style.right = xy[1] + 'px';
+  //   a[i].style.bottom = xy[0] + 'px';
+  //
+  //   next = i+1
+  //   console.log(next)
+  // }
+  //
+  // let rect1 = a[0]
+  // let rect2 = a[1]
+  //
+  //
+  // let overlap = !(rect1.top <= rect2.bottom && rect1.bottom >= rect2.top && rect1.left <= rect2.right && rect1.right >= rect2.left)
+  //
+  //
+  // console.log(rect1);
+  // console.log(rect2);
 
 
-
-  let i = 0
-  let next = 1
-  for (i = 0; i < a.length-1; i++) {
-    var xy = getRandomPosition(ballsWrapper);
-
-
-
-    a[i].style.width = mainBall.width;
-
-    a[i].style.top = xy[0] + 'px';
-    a[i].style.left = xy[1] + 'px';
-    a[i].style.right = xy[1] + 'px';
-    a[i].style.bottom = xy[0] + 'px';
-
-    next = i+1
-    console.log(next);
-
-    console.log( a[i+1].style.right)
-    if ( a[i].style.right < a[next].style.left ||
-         a[i].style.left > a[next].style.right ||
-         a[i].style.bottom < a[next].style.top ||
-         a[i].style.top > a[next].style.bottom) {
-
-        console.log('noover')
-      } else {
-        console.log('over')
-      }
-
-
-    }
 
 }
+
 
 
 // let ball1 = document.getElementById('ball1')
