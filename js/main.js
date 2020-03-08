a19ba437ff9cf1163eac25d3b3496f09ced7bca4
AnitaMigacheva <55283508+AnitaMigacheva@users.noreply.github.com>
2020-03-08 19:50:50 +0300

    -

--- js/main.js
+++ js/main.js
@@ -44,17 +44,41 @@ function showBalls() {
     var y = document.body.offsetWidth-element.clientWidth;
     var randomX = Math.floor(Math.random()*x);
     var randomY = Math.floor(Math.random()*y);
-    return [randomX,randomY];
+    return [randomX,randomY]
   }
 
 
 
-  for (let i = 0; i < a.length; i++) {
+  let i = 0
+  let next = 1
+  for (i = 0; i < a.length-1; i++) {
     var xy = getRandomPosition(ballsWrapper);
+
+
+
+    a[i].style.width = mainBall.width;
+
     a[i].style.top = xy[0] + 'px';
     a[i].style.left = xy[1] + 'px';
+    a[i].style.right = xy[1] + 'px';
+    a[i].style.bottom = xy[0] + 'px';
 
-  }
+    next = i+1
+    console.log(next);
+
+    console.log( a[i+1].style.right)
+    if ( a[i].style.right < a[next].style.left ||
+         a[i].style.left > a[next].style.right ||
+         a[i].style.bottom < a[next].style.top ||
+         a[i].style.top > a[next].style.bottom) {
+
+        console.log('noover')
+      } else {
+        console.log('over')
+      }
+
+
+    }
 
 }
 
