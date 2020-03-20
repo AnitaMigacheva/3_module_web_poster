884ac6e48396b7f8ef1e3c848f93a07cc8b0cf29
AnitaMigacheva <55283508+AnitaMigacheva@users.noreply.github.com>
2020-03-21 00:41:18 +0300

    -

--- js/main.js
+++ js/main.js
@@ -4,42 +4,54 @@ let bottle = document.body.querySelector('.bottle')
 let liquid = document.body.querySelector('.liquid')
 let bonesWrapper = document.body.querySelector('.bones')
 let collar = document.getElementById('collar_img')
+let wrapper2 = document.querySelector('.wrapper2')
 
-// $(document).ready(function () {
-//   //initialize swiper when document ready
-//   var mySwiper = new Swiper ('.swiper-container', {
-//     // Optional parameters
-//     direction: 'horizontal',
-//     loop: true
-//   })
-// });
+let items = document.body.querySelector('.items')
 
+$(window).resize(function() {
+  if (window.innerWidth < 600 ){
+     items.remove()
+  } else {
+    wrapper2.append(items)
+  }
+});
+
+
+$(document).ready(function () {
+  //initialize swiper when document ready
+  var swiper = new Swiper('.swiper-container', {
+    spaceBetween: 30,
+    centeredSlides: true,
+    preventClicks: false,
+    preventClicksPropagation: false,
+    autoplay: {
+      delay: 1000,
+      disableOnInteraction: true,
+    }
+  });
+});
 
 let swiperWrapper = document.querySelector('.swiper-wrapper')
 swiperWrapper.addEventListener('click', function(event){
-  if(event.target.className === 'liquid') {
-    liquid.style.opacity = '1'
-    liquid.style.transform = 'scale(1)'
+  if(event.target.classList.contains('bottle')) {
+    spill()
+  } else if (event.target.classList.contains('collar')) {
+    changeCollar()
   }
 })
 
 
+let images = new Array()
 
-
-let images = new Array();
-
-  let fishImg = './img/fish.png';
-
-   let i = 0;
-
-   images[0] = './img/collar-red.svg';
-   images[1] = './img/collar-gold.svg';
-   images[2] = './img/collar-black.svg';
-   images[3] = './img/collar-red.svg';
-   images[4] = './img/collar-wat.svg';
-   images[5] = './img/collar-eyes.svg';
-   images[6] = './img/fish.png';
-
+let fishImg = './img/fish.png'
+let i = 0
+images[0] = './img/collar-red.svg'
+images[1] = './img/collar-gold.svg'
+images[2] = './img/collar-black.svg'
+images[3] = './img/collar-red.svg'
+images[4] = './img/collar-wat.svg'
+images[5] = './img/collar-eyes.svg'
+images[6] = './img/fish.png'
 
 function changeCollar() {
    collar.src = images[i];
@@ -50,7 +62,6 @@ function changeCollar() {
    }
    setTimeout("changeCollar()", 800);
 }
-
 
 
 //------------------ОТКРЫВАНИЕ БАНОЧКИ--------------------
@@ -114,8 +125,8 @@ function getRandomPosition(element) {
 
 let balls = new Array()
 let delay = 90;
-let btnCreatCirc = document.querySelector('.ball')
-btnCreatCirc.onclick = function () {
+let btnCreatBall = document.querySelector('.ball')
+btnCreatBall.onclick = function() {
 
 
 
@@ -155,189 +166,3 @@ btnCreatCirc.onclick = function () {
   balls[9].src = './img/face.png'
 
 }
-
-
-//
-//
-//
-// let balls = new Array()
-//
-// function showBalls() {
-//
-//   let i = 0;
-//   for (i = 0; i < 30; i++) {
-//      balls[i] = document.createElement('img')
-//      balls[i].src = './img/ball.svg'
-//      balls[i].style.position = 'fixed'
-//      balls[i].style.display = 'block'
-//      balls[i].style.opacity = '1'
-//      balls[i].classList.add('new_ball')
-//      balls[i].style.transform = 'rotate(360deg)'
-//      balls[i].style.width = '16vw'
-//      let xy = getRandomPosition(balls[i])
-     // balls[i].style.top = xy[0] + 'px'
-     // balls[i].style.left = xy[1] + 'px'
-     // balls[i].style.right = xy[1] + 'px'
-     // balls[i].style.bottom = xy[0] + 'px'
-//
-//      document.body.append(balls[i])
-//      console.log(balls[i].style.display)
-//
-//      balls[i].addEventListener('mouseenter', function() {
-//          balls[i].style.display = 'none'
-//     })
-// modern browsers
-
-
-     // console.log( p.getBoundingClientRect());
-
-
-  //
-  // for (let j = i + 1; j < balls.length-1; j++) {
-  //
-  //
-  //    let prev = balls[i]
-  //    let next = balls[j]
-  //    console.log(prev);
-  //    console.log(next);
-  //
-  // //    let rr = prev.getBoundingClientRect();
-  // //    let bb = next.getBoundingClientRect();
-  // //
-  // //    console.log(rr);
-  // //    console.log(bb);
-  // //
-  // //
-  // //    let noOverlap = (rr.right < bb.left ||
-  // //                     rr.left > bb.right ||
-  // //                     rr.bottom < bb.top ||
-  // //                     rr.top > bb.bottom)
-  // //
-  // //    if (noOverlap) {
-  // //
-  // //      // sortedBalls.push(i)
-  // //      balls[i].style.opacity = '1';
-  // //
-  // //
-  // //    }
-  // //
-  // //
-  // //  console.log(noOverlap);
-  // //
-  // }
-//   }
-//   balls[0].src = './img/ball-text.svg'
-//   balls[1].src = './img/moon.png'
-//   balls[2].src = './img/face.png'
-//   balls[3].src = './img/ball-text.svg'
-//   balls[4].src = './img/moon.png'
-//   balls[5].src = './img/ball-text.svg'
-//   balls[6].src = './img/face.png'
-//
-//
-//
-// }
-
-
-// for (let j = i + 1; j < balls.length; j++) {
-//
-//
-//    let prev = balls[i]
-//    let next = balls[j]
-//    console.log(prev);
-//    console.log(next);
-//
-//    let rr = prev.getBoundingClientRect();
-//    let bb = next.getBoundingClientRect();
-//
-//    console.log(rr);
-//    console.log(bb);
-//
-//
-//    let noOverlap = (rr.right < bb.left ||
-//                     rr.left > bb.right ||
-//                     rr.bottom < bb.top ||
-//                     rr.top > bb.bottom)
-//
-//    if (noOverlap) {
-//
-//      // sortedBalls.push(i)
-//      balls[i].style.opacity = '1';
-//
-//
-//    }
-//
-//
-//  console.log(noOverlap);
-//
-// }
-
-
-
-
-
-
-//------------ТУТ ПРОВЕРКА OVERLAP JQUERY----------------
-
-
-// let elements = $(".boxes")
-// let x = 60
-// let dx = 60
-//
-// var boxDims = new Array();
-//
-// function setRandomPos(elements,x,dx) {
-// console.log(elements);
-//  elements.each(function(){
-//   var conflict = true;
-//   while (conflict) {
-//       fixLeft=(Math.floor(Math.random()*x)*20) + dx;
-//       fixTop = (Math.floor(Math.random()*40)*20) + 180;
-//       $(this).offset({
-//           left: fixLeft,
-//           top: fixTop
-//       });
-//       var box = {
-//           top: parseInt(window.getComputedStyle($(this)[0]).top),
-//           left: parseInt(window.getComputedStyle($(this)[0]).left),
-//           width: parseInt(window.getComputedStyle($(this)[0]).width),
-//           height: parseInt(window.getComputedStyle($(this)[0]).height)
-//       }
-//       conflict = false;
-//       for (var i = 0; i < boxDims.length; i++) {
-//
-//           if (overlap(box, boxDims[i])) {
-//               conflict = true;
-//               // box.style.display = "none"
-//           } else {
-//               conflict = false;
-//           }
-//       }
-//   }
-//   boxDims.push(box)
-//   // box.style.display = 'block'
-//
-// });
-// }
-//
-// function overlap(box1,box2) {
-//   var x1 = box1.left
-//   var y1 = box2.top;
-//   var h1 = box1.height;
-//   var w1 = box1.width;
-//   var b1 = y1 + h1;
-//   var r1 = x1 + w1;
-//   var x2 = box1.left;
-//   var y2 = box1.top;
-//   var h2 = box1.height;
-//   var w2 = box1.width;
-//   var b2 = y2 + h2;
-//   var r2 = x2 + w2;
-//
-//
-//
-//   if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2 ) return false;
-//   return true;
-// }
-//
-// setRandomPos($(".boxes"),40,40);
